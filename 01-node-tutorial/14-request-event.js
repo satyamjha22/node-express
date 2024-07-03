1928a8e74b60ed50d5c28ad2c46397350ad64515
alasaidi <alasaidi55gmail.com>
2024-07-03 19:34:13 +0200

    node tuto done

--- 01-node-tutorial/14-request-event.js
+++ 01-node-tutorial/14-request-event.js
@@ -9,7 +9,6 @@ const server = http.createServer();
 // emits request event
 // subcribe to it / listen for it / respond to it
 server.on("request", (req, res) => {
-  res.end("Welcome");
+  res.end("Request received");
 });
-
 server.listen(5000);
--- 01-node-tutorial/15-create-big-file.js
+++ 01-node-tutorial/15-create-big-file.js
@@ -1,4 +1,4 @@
 const { writeFileSync } = require("fs");
-for (let i = 0; i < 10000; i++) {
-  writeFileSync("./content/big.txt", `hello world ${i}\n`, { flag: "a" });
+for (i = 0; i < 10000; i++) {
+  writeFileSync("./content/subfolder/big-file.txt", `hello word ${i}`, { flag: "a" });
 }
