a3cf076c2ed898ae35eadc29fd434e94969314a2
alasaidi <alasaidi55gmail.com>
2024-07-05 11:52:02 +0200

    statusCode and resources pages

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,17 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
-    res.end("hello word");
-    console.log(req);
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>hello world</h1>");
+    res.end();
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about</h1>");
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>404 not found</h1>");
+    res.end();
   }
 });
 
