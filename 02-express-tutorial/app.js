6c3f7066baffb198a3c3925ca4ea78cfc80dec4b
alasaidi <alasaidi55gmail.com>
2024-07-05 11:01:14 +0200

    update http file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("hello word");
+    console.log(req);
+  }
+});
+
+server.listen(5000);
