a7fa4b5f7c6fc9a13a220254c05a3dc8350151f5
alasaidi <alasaidi55gmail.com>
2024-07-09 11:56:11 +0200

    static and middleware

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -10,3 +10,30 @@ const server = http.createServer((req, res) => {
   }
 });
 server.listen(5000);
+// http
+//   .createServer((req, res) => {
+//     const data = { name: "John", age: 30 };
+
+//     res.setHeader("Content-Type", "application/json");
+//     res.end(JSON.stringify(data));
+//   })
+//   .listen(3000);
+
+// const server = http.createServer((req, res) => {
+// Set headers
+//   res.setHeader("Content-Type", "text/html");
+//   res.setHeader("X-Custom-Header", "Hello!");
+
+// Some processing might happen here
+
+// Set another header
+//   res.setHeader("Cache-Control", "max-age=3600");
+
+// End the response and send content
+//   res.end("<h1>Hello, World!</h1>");
+
+// This would throw an error because the response is already sent
+// res.setHeader('X-Late-Header', 'Too late!');
+// });
+
+// server.listen(3000);
--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,58 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>hello world</h1>");
-    res.end();
-  } else if (req.url == "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about</h1>");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>404 not found</h1>");
-    res.end();
-  }
+// const http = require("http");
+// const { readFileSync } = require("fs");
+// // reading the file
+// const homePage = readFileSync("./navbar-app/index.html");
+// const stylePage = readFileSync("./navbar-app/styles.css");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url == "/") {
+//     res.writeHead(200, { "content-type": "text/html" });
+//     res.write(homePage);
+//     res.end();
+//   }
+
+//   if (req.url == "/styles.css") {
+//     res.writeHead(200, { "content-type": "text/css" });
+//     res.write(stylePage);
+//     res.end();
+//   }
+
+// } else if (req.url == "/about") {
+//   res.writeHead(200, { "content-type": "text/html" });
+//   res.write("<h1>about</h1>");
+//   res.end();
+// } else {
+//   res.writeHead(404, { "content-type": "text/html" });
+//   res.write("<h1>404 not found</h1>");
+//   res.end();
+// }
+// });
+
+// server.listen(5000);
+
+//------------------------------------------------------------------------------------------------//
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const index = path.resolve(__dirname, "./navbar-app/index.html");
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(index);
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  console.log("about page ");
+  res.send("about page ");
+});
+
+app.all("*", (req, res) => {
+  console.log("404");
+  res.status(404).send("page not found  ");
+});
+
+app.listen(5000, () => {
+  console.log("server is running on port 5000");
+});
