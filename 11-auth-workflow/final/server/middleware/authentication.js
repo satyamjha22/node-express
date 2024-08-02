ff2aaa980dea1de5b0748968278a824e8bc5704f
jydxh <wzxxds@gmail.com>
2024-08-02 10:19:08 -0400

    format

--- 11-auth-workflow/final/server/middleware/authentication.js
+++ 11-auth-workflow/final/server/middleware/authentication.js
@@ -1,25 +1,25 @@
-const CustomError = require('../errors')
-const { isTokenValid } = require('../utils')
-const Token = require('../models/Token')
-const { attachCookiesToResponse } = require('../utils')
+const CustomError = require('../errors');
+const { isTokenValid } = require('../utils');
+const Token = require('../models/Token');
+const { attachCookiesToResponse } = require('../utils');
 const authenticateUser = async (req, res, next) => {
-	const { refreshToken, accessToken } = req.signedCookies
+	const { refreshToken, accessToken } = req.signedCookies;
 
 	try {
 		if (accessToken) {
-			const payload = await isTokenValid(accessToken)
-			req.user = payload.user
-			return next()
+			const payload = await isTokenValid(accessToken);
+			req.user = payload.user;
+			return next();
 		}
-		const payload = await isTokenValid(refreshToken)
+		const payload = await isTokenValid(refreshToken);
 
 		const existingToken = await Token.findOne({
 			user: payload.user.userId,
 			refreshToken: payload.refreshToken,
-		})
+		});
 
 		if (!existingToken || !existingToken?.isValid) {
-			throw new CustomError.UnauthenticatedError('Authentication Invalid')
+			throw new CustomError.UnauthenticatedError('Authentication Invalid');
 		}
 
 		await attachCookiesToResponse({
@@ -27,27 +27,27 @@ const authenticateUser = async (req, res, next) => {
 			user: payload.user,
 			refreshToken: existingToken.refreshToken,
 			expiresIn: existingToken.expiredIn,
-		})
+		});
 
-		req.user = payload.user
-		next()
+		req.user = payload.user;
+		next();
 	} catch (error) {
-		throw new CustomError.UnauthenticatedError('Authentication Invalid')
+		throw new CustomError.UnauthenticatedError('Authentication Invalid');
 	}
-}
+};
 
 const authorizePermissions = (...roles) => {
 	return (req, res, next) => {
 		if (!roles.includes(req.user.role)) {
 			throw new CustomError.UnauthorizedError(
 				'Unauthorized to access this route'
-			)
+			);
 		}
-		next()
-	}
-}
+		next();
+	};
+};
 
 module.exports = {
 	authenticateUser,
 	authorizePermissions,
-}
+};
--- 11-auth-workflow/final/server/utils/jwt.js
+++ 11-auth-workflow/final/server/utils/jwt.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken')
-const { promisify } = require('util')
-const jwtVerify = promisify(jwt.verify)
-const jwtSign = promisify(jwt.sign)
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const jwtVerify = promisify(jwt.verify);
+const jwtSign = promisify(jwt.sign);
 
 const createJWT = async ({ payload }) =>
 	await jwtSign(payload, process.env.JWT_SECRET, {
 		expiresIn: payload.expiresIn,
-	})
+	});
 
 const isTokenValid = async token =>
-	await jwtVerify(token, process.env.JWT_SECRET)
+	await jwtVerify(token, process.env.JWT_SECRET);
 
 const attachCookiesToResponse = async ({
 	res,
@@ -19,7 +19,7 @@ const attachCookiesToResponse = async ({
 }) => {
 	const accessTokenJWT = await createJWT({
 		payload: { user, expiresIn: 60 * 15 },
-	}) //set accessJWT 15 mins valid
+	}); //set accessJWT 15 mins valid
 	const refreshTokenJWT = await createJWT({
 		payload: {
 			user,
@@ -27,25 +27,25 @@ const attachCookiesToResponse = async ({
 			expiresIn: new Date(expiresIn).getTime() - Date.now() - 5 * 60,
 			// refreshJWT will be expired 5min ahead the expiredTime saved in DB
 		},
-	})
+	});
 
-	const oneDay = 1000 * 60 * 60 * 24
-	const longerExp = 1000 * 60 * 60 * 24 * 30
+	const oneDay = 1000 * 60 * 60 * 24;
+	const longerExp = 1000 * 60 * 60 * 24 * 30;
 
 	res.cookie('accessToken', accessTokenJWT, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
 		signed: true,
 		expires: new Date(Date.now() + 1000 * 60 * 15), //15min
-	})
+	});
 
 	res.cookie('refreshToken', refreshTokenJWT, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
 		signed: true,
 		expires: new Date(expiresIn - 1000 * 60 * 5), // if use Date.now() as reference, the refreshToken will never be expired, which is a bug, so add an expiredIn attribute in Token Schema, and use that as reference, whenever the refreshToken refresh, it's live can also be refreshed correct
-	})
-}
+	});
+};
 // const attachSingleCookieToResponse = ({ res, user }) => {
 //   const token = createJWT({ payload: user });
 
@@ -63,4 +63,4 @@ module.exports = {
 	createJWT,
 	isTokenValid,
 	attachCookiesToResponse,
-}
+};
