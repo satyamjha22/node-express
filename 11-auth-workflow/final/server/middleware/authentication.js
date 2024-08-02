0480f8b963df00155d67f8b5e79ed7bcda062c10
jydxh <wzxxds@gmail.com>
2024-08-02 10:11:30 -0400

    fix the refreshToken never expire bug, also make sure refreshToken will be changed and saved in DB everytime user log in

--- 11-auth-workflow/final/server/middleware/authentication.js
+++ 11-auth-workflow/final/server/middleware/authentication.js
@@ -1,52 +1,53 @@
-const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
-const Token = require('../models/Token');
-const { attachCookiesToResponse } = require('../utils');
+const CustomError = require('../errors')
+const { isTokenValid } = require('../utils')
+const Token = require('../models/Token')
+const { attachCookiesToResponse } = require('../utils')
 const authenticateUser = async (req, res, next) => {
-  const { refreshToken, accessToken } = req.signedCookies;
+	const { refreshToken, accessToken } = req.signedCookies
 
-  try {
-    if (accessToken) {
-      const payload = isTokenValid(accessToken);
-      req.user = payload.user;
-      return next();
-    }
-    const payload = isTokenValid(refreshToken);
+	try {
+		if (accessToken) {
+			const payload = await isTokenValid(accessToken)
+			req.user = payload.user
+			return next()
+		}
+		const payload = await isTokenValid(refreshToken)
 
-    const existingToken = await Token.findOne({
-      user: payload.user.userId,
-      refreshToken: payload.refreshToken,
-    });
+		const existingToken = await Token.findOne({
+			user: payload.user.userId,
+			refreshToken: payload.refreshToken,
+		})
 
-    if (!existingToken || !existingToken?.isValid) {
-      throw new CustomError.UnauthenticatedError('Authentication Invalid');
-    }
+		if (!existingToken || !existingToken?.isValid) {
+			throw new CustomError.UnauthenticatedError('Authentication Invalid')
+		}
 
-    attachCookiesToResponse({
-      res,
-      user: payload.user,
-      refreshToken: existingToken.refreshToken,
-    });
+		await attachCookiesToResponse({
+			res,
+			user: payload.user,
+			refreshToken: existingToken.refreshToken,
+			expiresIn: existingToken.expiredIn,
+		})
 
-    req.user = payload.user;
-    next();
-  } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
-  }
-};
+		req.user = payload.user
+		next()
+	} catch (error) {
+		throw new CustomError.UnauthenticatedError('Authentication Invalid')
+	}
+}
 
 const authorizePermissions = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      throw new CustomError.UnauthorizedError(
-        'Unauthorized to access this route'
-      );
-    }
-    next();
-  };
-};
+	return (req, res, next) => {
+		if (!roles.includes(req.user.role)) {
+			throw new CustomError.UnauthorizedError(
+				'Unauthorized to access this route'
+			)
+		}
+		next()
+	}
+}
 
 module.exports = {
-  authenticateUser,
-  authorizePermissions,
-};
+	authenticateUser,
+	authorizePermissions,
+}
--- 11-auth-workflow/final/server/models/Token.js
+++ 11-auth-workflow/final/server/models/Token.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const TokenSchema = new mongoose.Schema(
-  {
-    refreshToken: { type: String, required: true },
-    ip: { type: String, required: true },
-    userAgent: { type: String, required: true },
-    isValid: { type: Boolean, default: true },
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+	{
+		refreshToken: { type: String, required: true },
+		ip: { type: String, required: true },
+		userAgent: { type: String, required: true },
+		isValid: { type: Boolean, default: true },
+		user: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		expiredIn: { type: Date, required: true },
+	},
+	{ timestamps: true }
+)
 
-module.exports = mongoose.model('Token', TokenSchema);
+module.exports = mongoose.model('Token', TokenSchema)
